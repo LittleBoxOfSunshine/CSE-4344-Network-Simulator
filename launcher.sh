5e4a84484f0272d1a265faba729602cbec793d52
Matthew Lee <matthew.lee711@gmail.com>
2016-04-25 20:57:29 -0500

    getting merge ready for launch.sh

--- launcher.sh
+++ launcher.sh
@@ -6,11 +6,21 @@ echo "How many iterations would you like to generate?"
 read input
 filename="input.txt"
 
-for ((i=1;i<=input;i++)); do 
-	while read -r line
-	do
-	    name="$line"
-	    echo "Name read from file - $name"
-	    ./a "$name"
-	done < "$filename"	
+settings = "$1Settings"
+message = "$1Messages"
+output = "$1Log"
+low = $2
+high = $3
+
+# for ((i=1;i<=input;i++)); do 
+# 	while read -r line
+# 	do
+# 	    name="$line"
+# 	    echo "Name read from file - $name"
+# 	    ./a.out "$name"
+# 	done < "$filename"	
+# done
+
+for ((i=$low;i<=$high;i++)); do
+	./a.out "$settings$i" "$message$i" "$output$i"
 done
