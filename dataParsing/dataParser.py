ec9d5bad38fff879eea9e7bed531cb94c0176a5b
david <dykim@smu.edu>
2016-05-02 12:33:40 -0500

    added min, max, stdv and output to a .csv

--- dataParsing/dataParser.py
+++ dataParsing/dataParser.py
@@ -13,30 +13,30 @@ numDestString = "Number of Destinations: "
 latencyString = "has reached destination node"
 activeTicksString = "Total number of active ticks: "
 
-iterations = 1000		#Num of iterations of log loops
+iterations = 2		#Num of iterations of log loops
 
 usingLnc = {}
 noLnc = {}
 
-usingLnc["totalTicks"] = [0, 0]	#[value, # of times this value was taken]
-usingLnc["tickPacket"] = [0, 0]
-usingLnc["ctsEmit"] = [0, 0]
-usingLnc["rtsEmit"] = [0, 0]
-usingLnc["ctsReceive"] = [0, 0]
-usingLnc["rtsReceive"] = [0, 0]
-usingLnc["numDest"] = [0, 0]
-usingLnc["latency"] = [0, 0]
-usingLnc["activeTicks"] = [0, 0]
+usingLnc["totalTicks"] = []	#[value, # of times this value was taken]
+usingLnc["tickPacket"] = []
+usingLnc["ctsEmit"] = []
+usingLnc["rtsEmit"] = []
+usingLnc["ctsReceive"] = []
+usingLnc["rtsReceive"] = []
+usingLnc["numDest"] = []
+usingLnc["latency"] = []
+usingLnc["activeTicks"] = []
 
-noLnc["totalTicks"] = [0, 0]
-noLnc["tickPacket"] = [0, 0]
-noLnc["ctsEmit"] = [0, 0]
-noLnc["rtsEmit"] = [0, 0]
-noLnc["ctsReceive"] = [0, 0]
-noLnc["rtsReceive"] = [0, 0]
-noLnc["numDest"] = [0, 0]
-noLnc["latency"] = [0, 0]
-noLnc["activeTicks"] = [0, 0]
+noLnc["totalTicks"] = []
+noLnc["tickPacket"] = []
+noLnc["ctsEmit"] = []
+noLnc["rtsEmit"] = []
+noLnc["ctsReceive"] = []
+noLnc["rtsReceive"] = []
+noLnc["numDest"] = []
+noLnc["latency"] = []
+noLnc["activeTicks"] = []
 
 fileType = sys.argv[1]
 for x in range(1, iterations+1):
@@ -52,69 +52,59 @@ for x in range(1, iterations+1):
 			lnc = False
 		if lnc:
 			if tickString in line:
-				usingLnc["totalTicks"][0] += int(line[len(tickString):])
-				usingLnc["totalTicks"][1] += 1
+				usingLnc["totalTicks"].append(int(line[len(tickString):]))
 			elif latencyString in line:
-				usingLnc["latency"][0] += int(line[line.find("in ")+3:line.find("ticks")])
-				usingLnc["latency"][1] += 1
+				usingLnc["latency"].append(int(line[line.find("in ")+3:line.find("ticks")]))
 			elif numDestString in line:
-				usingLnc["numDest"][0] += int(line[len(numDestString):])
-				usingLnc["numDest"][1] += 1
+				usingLnc["numDest"].append(int(line[len(numDestString):]))
 			elif tickPacketString in line:
-				usingLnc["tickPacket"][0] += int(line[len(tickPacketString):])
-				usingLnc["tickPacket"][1] += 1
+				usingLnc["tickPacket"].append(int(line[len(tickPacketString):]))
 			elif ctsEmitString in line:
-				usingLnc["ctsEmit"][0] += int(line[len(ctsEmitString):])
-				usingLnc["ctsEmit"][1] += 1
+				usingLnc["ctsEmit"].append(int(line[len(ctsEmitString):]))
 			elif rtsEmitString in line:
-				usingLnc["rtsEmit"][0] += int(line[len(rtsEmitString):])
-				usingLnc["rtsEmit"][1] += 1
+				usingLnc["rtsEmit"].append(int(line[len(rtsEmitString):]))
 			elif ctsReceiveString in line:
-				usingLnc["ctsReceive"][0] += int(line[len(ctsReceiveString):])
-				usingLnc["ctsReceive"][1] += 1
+				usingLnc["ctsReceive"].append(int(line[len(ctsReceiveString):]))
 			elif rtsReceiveString in line:
-				usingLnc["rtsReceive"][0] += int(line[len(ctsReceiveString):])
-				usingLnc["rtsReceive"][1] += 1
+				usingLnc["rtsReceive"].append(int(line[len(ctsReceiveString):]))
 			elif activeTicksString in line:
-				usingLnc["activeTicks"][0] += int(line[len(activeTicksString):])
-				usingLnc["activeTicks"][1] += 1
+				usingLnc["activeTicks"].append(int(line[len(activeTicksString):]))
 			elif endSimulatorString in line:
 					lnc = False
 		else:
 			if tickString in line:
-				noLnc["totalTicks"][0] += int(line[len(tickString):])
-				noLnc["totalTicks"][1] += 1
+				noLnc["totalTicks"].append(int(line[len(tickString):]))
 			elif latencyString in line:
-				noLnc["latency"][0] += int(line[line.find("in ")+3:line.find("ticks")])
-				noLnc["latency"][1] += 1
+				noLnc["latency"].append(int(line[line.find("in ")+3:line.find("ticks")]))
 			elif numDestString in line:
-				noLnc["numDest"][0] += int(line[len(numDestString):])
-				noLnc["numDest"][1] += 1
+				noLnc["numDest"].append(int(line[len(numDestString):]))
 			elif tickPacketString in line:
-				noLnc["tickPacket"][0] += int(line[len(tickPacketString):])
-				noLnc["tickPacket"][1] += 1
+				noLnc["tickPacket"].append(int(line[len(tickPacketString):]))
 			elif ctsEmitString in line:
-				noLnc["ctsEmit"][0] += int(line[len(ctsEmitString):])
-				noLnc["ctsEmit"][1] += 1
+				noLnc["ctsEmit"].append(int(line[len(ctsEmitString):]))
 			elif rtsEmitString in line:
-				noLnc["rtsEmit"][0] += int(line[len(rtsEmitString):])
-				noLnc["rtsEmit"][1] += 1
+				noLnc["rtsEmit"].append(int(line[len(rtsEmitString):]))
 			elif ctsReceiveString in line:
-				noLnc["ctsReceive"][0] += int(line[len(ctsReceiveString):])
-				noLnc["ctsReceive"][1] += 1
+				noLnc["ctsReceive"].append(int(line[len(ctsReceiveString):]))
 			elif rtsReceiveString in line:
-				noLnc["rtsReceive"][0] += int(line[len(ctsReceiveString):])
-				noLnc["rtsReceive"][1] += 1
+				noLnc["rtsReceive"].append(int(line[len(ctsReceiveString):]))
 			elif activeTicksString in line:
-				noLnc["activeTicks"][0] += int(line[len(activeTicksString):])
-				noLnc["activeTicks"][1] += 1
+				noLnc["activeTicks"].append(int(line[len(activeTicksString):]))
 			elif endSimulatorString in line:
 				lnc = True
 	dataFile.close()
-print "Using LNC Averages"
-for x in usingLnc.keys():
-	print x + " " + str(usingLnc[x][0]/float(usingLnc[x][1]))
 
-print "\nNot Using LNC Averages"
+print usingLnc["totalTicks"]
+mean = lambda MyList : reduce(lambda x, y: x + y, MyList) / float(len(MyList))
+stdv = lambda MyList : (reduce(lambda x,y : x + y , map(lambda x: (x-mean(MyList))**2 , MyList)) / float(len(MyList)))**.5
+writeFile = open(fileType + "Results.csv", 'w')
+writeFile.write("Using LNC,Mean,Min,Max,STDV\n")
+for x in usingLnc.keys():
+	write =  x + "," + str(mean(usingLnc[x])) + "," + str(min(usingLnc[x])) + "," + str(max(usingLnc[x])) + "," + str(stdv(usingLnc[x]))
+	writeFile.write(write+"\n")
+
+writeFile.write("\nNo LNC,Mean,Min,Max,STDV\n")
 for x in noLnc.keys():
-	print x + " " + str(noLnc[x][0]/float(noLnc[x][1]))
+	write =  x + ", " + str(mean(noLnc[x])) + "," + str(min(noLnc[x])) + "," + str(max(noLnc[x])) + "," + str(stdv(noLnc[x]))
+	writeFile.write(write+"\n")
+writeFile.close()
